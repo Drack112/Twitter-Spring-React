314747c76ecf9cf2789954644d7e866800301b0a
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-04-23 15:44:51 -0300

    feat: constants for the shared application

---
+++ commons/src/main/java/com/gmail/drack/commons/constants/HeaderConstants.java
@@ -0,0 +1,7 @@
+package com.gmail.drack.commons.constants;
+
+public class HeaderConstants {
+
+    public static final String AUTH_USER_ID_HEADER = "X-auth-user-id";
+    public static final String PAGE_TOTAL_COUNT = "page-total-count";
+}
