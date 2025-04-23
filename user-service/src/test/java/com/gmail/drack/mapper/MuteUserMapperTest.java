964c5524dddd5ca32d764dd06c6f5698a3573e19
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-04-23 16:24:16 -0300

    test: mute user feature

---
+++ user-service/src/test/java/com/gmail/drack/mapper/MuteUserMapperTest.java
@@ -0,0 +1,58 @@
+package com.gmail.drack.mapper;
+
+import com.gmail.drack.UserServiceTestHelper;
+import com.gmail.drack.commons.dto.HeaderResponse;
+import com.gmail.drack.commons.mapper.BasicMapper;
+import com.gmail.drack.dto.response.MutedUserResponse;
+import com.gmail.drack.repository.projection.MutedUserProjection;
+import com.gmail.drack.service.MuteUserService;
+import com.gmail.drack.commons.util.TestConstants;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class MuteUserMapperTest {
+
+    @InjectMocks
+    private MuteUserMapper muteUserMapper;
+
+    @Mock
+    private MuteUserService muteUserService;
+
+    @Mock
+    private BasicMapper basicMapper;
+
+    private static final PageRequest pageable = PageRequest.of(0, 20);
+
+    @Test
+    public void getMutedList() {
+        Page<MutedUserProjection> mutedUserProjections = UserServiceTestHelper.createMutedUserProjections();
+        HeaderResponse<MutedUserResponse> headerResponse = new HeaderResponse<>(
+                List.of(new MutedUserResponse(), new MutedUserResponse()), new HttpHeaders());
+        when(muteUserService.getMutedList(pageable)).thenReturn(mutedUserProjections);
+        when(basicMapper.getHeaderResponse(mutedUserProjections, MutedUserResponse.class)).thenReturn(headerResponse);
+        assertEquals(headerResponse, muteUserMapper.getMutedList(pageable));
+        verify(muteUserService, times(1)).getMutedList(pageable);
+    }
+
+    @Test
+    public void processMutedList() {
+        when(muteUserService.processMutedList(TestConstants.USER_ID)).thenReturn(true);
+        assertTrue(muteUserMapper.processMutedList(TestConstants.USER_ID));
+        verify(muteUserService, times(1)).processMutedList(TestConstants.USER_ID);
+    }
+}
---
+++ user-service/src/test/java/com/gmail/drack/service/MuteUserServiceImplTest.java
@@ -0,0 +1,73 @@
+package com.gmail.drack.service;
+
+import com.gmail.drack.UserServiceTestHelper;
+import com.gmail.drack.commons.exception.ApiRequestException;
+import com.gmail.drack.constants.UserErrorMessage;
+import com.gmail.drack.model.User;
+import com.gmail.drack.repository.projection.MutedUserProjection;
+import com.gmail.drack.commons.util.TestConstants;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class MuteUserServiceImplTest extends AbstractServiceTest {
+
+    @Autowired
+    private MuteUserService muteUserService;
+
+    private User user;
+
+    @Before
+    public void setUp() {
+        super.setUp();
+        user = new User();
+        user.setId(1L);
+    }
+
+    @Test
+    public void getMutedList_ShouldReturnMutedUserProjections() {
+        Page<MutedUserProjection> mutedUserProjection = UserServiceTestHelper.createMutedUserProjections();
+        when(muteUserRepository.getUserMuteListById(TestConstants.USER_ID, pageable)).thenReturn(mutedUserProjection);
+        assertEquals(mutedUserProjection, muteUserService.getMutedList(pageable));
+        verify(muteUserRepository, times(1)).getUserMuteListById(TestConstants.USER_ID, pageable);
+    }
+
+    @Test
+    public void processMutedList_ShouldMuteUser() {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(muteUserRepository.isUserMuted(TestConstants.USER_ID, 1L)).thenReturn(false);
+        assertTrue(muteUserService.processMutedList(1L));
+        verify(userRepository, times(1)).findById(1L);
+        verify(muteUserRepository, times(1)).isUserMuted(TestConstants.USER_ID, 1L);
+        verify(muteUserRepository, times(1)).muteUser(TestConstants.USER_ID, 1L);
+        verify(muteUserProducer, times(1)).sendMuteUserEvent(user, TestConstants.USER_ID, true);
+    }
+
+    @Test
+    public void processMutedList_ShouldUnmuteUser() {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(muteUserRepository.isUserMuted(TestConstants.USER_ID, 1L)).thenReturn(true);
+        assertFalse(muteUserService.processMutedList(1L));
+        verify(userRepository, times(1)).findById(1L);
+        verify(muteUserRepository, times(1)).isUserMuted(TestConstants.USER_ID, 1L);
+        verify(muteUserRepository, times(1)).unmuteUser(TestConstants.USER_ID, 1L);
+        verify(muteUserProducer, times(1)).sendMuteUserEvent(user, TestConstants.USER_ID, false);
+    }
+
+    @Test
+    public void processMutedList_ShouldUserNotFound() {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+        ApiRequestException exception = assertThrows(ApiRequestException.class,
+                () -> muteUserService.processMutedList(1L));
+        assertEquals(String.format(UserErrorMessage.USER_ID_NOT_FOUND, 1L), exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+        verify(userRepository, times(1)).findById(1L);
+    }
+}
