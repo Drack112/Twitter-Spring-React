b5cd34304ce0f33af2f028a8530cac54185bae95
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-04-23 16:21:38 -0300

    feat: configuration to client on other application

---
+++ user-service/src/main/java/com/gmail/drack/client/TweetClient.java
@@ -0,0 +1,25 @@
+package com.gmail.drack.client;
+
+import com.gmail.drack.commons.configuration.FeignConfiguration;
+import com.gmail.drack.commons.constants.FeignConstants;
+import com.gmail.drack.commons.constants.PathConstants;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = FeignConstants.TWEET_SERVICE, path = PathConstants.API_V1_TWEETS, configuration = FeignConfiguration.class)
+public interface TweetClient {
+
+    @CircuitBreaker(name = FeignConstants.TWEET_SERVICE)
+    @GetMapping(PathConstants.ID_TWEET_ID)
+    Boolean isTweetExists(@PathVariable("tweetId") Long tweetId);
+
+    @CircuitBreaker(name = FeignConstants.TWEET_SERVICE, fallbackMethod = "defaultTweetCount")
+    @GetMapping(PathConstants.COUNT_TEXT)
+    Long getTweetCountByText(@PathVariable("text") String text);
+
+    default Long defaultTweetCount(Throwable throwable) {
+        return 0L;
+    }
+}
